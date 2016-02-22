91a37c3be6023e7beff9505a23659a5578ded11d
Kystek <kystek@gmail.com>
2016-02-22 17:42:23 +0100

    Starter changed

--- src/starter/Starter.java
+++ src/starter/Starter.java
@@ -3,8 +3,6 @@ package starter;
 import java.awt.EventQueue;
 
 import businessLogic.viewManagers.ConnectionWindowManager;
-import businessLogic.viewManagers.ManageWindowManager;
-import dataLayer.LoginInformation;
 
 public class Starter {
 
@@ -15,24 +13,9 @@ public class Starter {
 	public Starter() {		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
-//				new ConnectionWindowManager().show();
-				new ManageWindowManager(new LoginInformation("user", "logitech1")).show();
+				new ConnectionWindowManager().show();
 			}
 		});
-		
-		
-		/*CustomerManager customerManager = new CustomerManager();
-		BankAccountManager bankAccountManager = new BankAccountManager();
-		try {
-			bankAccountManager.connect(new LoginInformation("user", "logitech1"));
-			bankAccountManager.create(1243, 12443, 12341234L);
-			bankAccountManager.closeConnection();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}*/
-		
 	}	
 	
 }
